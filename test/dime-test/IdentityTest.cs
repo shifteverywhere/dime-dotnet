61795c9d06509f16105a2208c15b84631703473d
Niclas Kjellin <niclas@shifteverwhere.com>
2021-05-18 20:18:58 +0200

    Initial project/solution structure

---
+++ test/dime-test/IdentityTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dime_test
+{
+    [TestClass]
+    public class IdentityTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
