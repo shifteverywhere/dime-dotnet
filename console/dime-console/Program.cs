01618d88205d9181e07aba08ff0a434353302fe2
Niclas Kjellin <niclas@shifteverywhere.com>
2021-05-25 20:17:07 +0200

    Some refactoring of Envelope

--- console/dime-console/Program.cs
+++ console/dime-console/Program.cs
@@ -34,7 +34,9 @@ namespace ShiftEverywhere.DiMEConsole
         public Message GenerateMessage(Guid subjectId, Identity issuerIdentity, string payload)
         {
             long expiresAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds() + 120;
-            return new Message(subjectId, issuerIdentity, Encoding.UTF8.GetBytes(payload), expiresAt);
+            Message msg = new Message(subjectId, issuerIdentity, expiresAt);
+            msg.AddPayload(Encoding.UTF8.GetBytes(payload));
+            return msg;
         }
 
         static void Main(string[] args)
@@ -45,28 +47,31 @@ namespace ShiftEverywhere.DiMEConsole
             string serviceProviderMessageEncoded = serviceProviderMessage.Export(prg.serviceProviderKeypair.PrivateKey);
             // Send 'serviceProviderMessageEncoded' to back-end
             Message serviceProviderMessageAtBackEnd = Message.Import(serviceProviderMessageEncoded);
-            Envelope backEndEnvelope = new Envelope(prg.trustedIdentity, prg.mobileIdentity.SubjectId, DateTimeOffset.UtcNow.ToUnixTimeSeconds(), serviceProviderMessage.ExpiresAt);
+            Envelope backEndEnvelope = new Envelope(prg.trustedIdentity, prg.mobileIdentity.SubjectId, 120);
             backEndEnvelope.AddMessage(serviceProviderMessage);
-            string backEndEnvelopeEncoded = backEndEnvelope.Export(prg.trustedKeypair.PrivateKey);
+            backEndEnvelope.Seal(prg.trustedKeypair.PrivateKey);
+            string backEndEnvelopeEncoded = backEndEnvelope.Export();
             // Send 'backEndEnvelopeEncoded' to mobile
             Envelope backEndEnvelopeAtMobile = Envelope.Import(backEndEnvelopeEncoded);
-            string messagePayload = System.Text.Encoding.UTF8.GetString(backEndEnvelopeAtMobile.messages[0].GetPayload(), 0, backEndEnvelopeAtMobile.messages[0].GetPayload().Length);
+            string messagePayload = System.Text.Encoding.UTF8.GetString(backEndEnvelopeAtMobile.Messages[0].GetPayload(), 0, backEndEnvelopeAtMobile.Messages[0].GetPayload().Length);
             Console.WriteLine("Message from service provider: " + messagePayload);
             Message mobileResponseMessage = prg.GenerateMessage(prg.mobileIdentity.SubjectId, prg.serviceProviderIdentity, "Yes, it is!");
-            mobileResponseMessage.LinkMessage(backEndEnvelopeAtMobile.messages[0]); // link the mobile response to the received service provider message       
-            Envelope mobileEnvelope = new Envelope(prg.mobileIdentity, prg.serviceProviderIdentity.IssuerId, mobileResponseMessage.IssuedAt, backEndEnvelopeAtMobile.expiresAt);
-            mobileEnvelope.AddMessage(backEndEnvelopeAtMobile.messages[0]);
+            mobileResponseMessage.LinkMessage(backEndEnvelopeAtMobile.Messages[0]); // link the mobile response to the received service provider message       
+            Envelope mobileEnvelope = new Envelope(prg.mobileIdentity, prg.serviceProviderIdentity.IssuerId, 120);
+            mobileEnvelope.AddMessage(backEndEnvelopeAtMobile.Messages[0]);
             mobileEnvelope.AddMessage(mobileResponseMessage);
-            string mobileEnvelopeEncoded = mobileEnvelope.Export(prg.mobileKeypair.PrivateKey);
+            mobileEnvelope.Seal(prg.mobileKeypair.PrivateKey);
+            string mobileEnvelopeEncoded = mobileEnvelope.Export();
             // Send 'mobileEnvelopeEncoded' to back-end
             Envelope mobleEnvelopeAtBackEnd = Envelope.Import(mobileEnvelopeEncoded);
-            Envelope finalBackEndEnvelope = new Envelope(prg.trustedIdentity, mobileEnvelope.subjectId, DateTimeOffset.UtcNow.ToUnixTimeSeconds(), mobileEnvelope.expiresAt);
-            finalBackEndEnvelope.AddMessage(mobleEnvelopeAtBackEnd.messages[0]);
-            finalBackEndEnvelope.AddMessage(mobleEnvelopeAtBackEnd.messages[1]);
-            string finalBackEndEnvelopeEncoded = finalBackEndEnvelope.Export(prg.trustedKeypair.PrivateKey);
+            Envelope finalBackEndEnvelope = new Envelope(prg.trustedIdentity, mobileEnvelope.SubjectId, 120);
+            finalBackEndEnvelope.AddMessage(mobleEnvelopeAtBackEnd.Messages[0]);
+            finalBackEndEnvelope.AddMessage(mobleEnvelopeAtBackEnd.Messages[1]);
+            finalBackEndEnvelope.Seal(prg.trustedKeypair.PrivateKey);
+            string finalBackEndEnvelopeEncoded = finalBackEndEnvelope.Export();
             // Send 'finalBackEndEnvelopeEncoded' to service provider
             Envelope finalBackEndEnvelopeAtServiceProvider = Envelope.Import(finalBackEndEnvelopeEncoded);
-            string responcePayload = System.Text.Encoding.UTF8.GetString(finalBackEndEnvelopeAtServiceProvider.messages[1].GetPayload(), 0, finalBackEndEnvelopeAtServiceProvider.messages[1].GetPayload().Length);
+            string responcePayload = System.Text.Encoding.UTF8.GetString(finalBackEndEnvelopeAtServiceProvider.Messages[1].GetPayload(), 0, finalBackEndEnvelopeAtServiceProvider.Messages[1].GetPayload().Length);
             Console.WriteLine("Responce from mobile: " + responcePayload);
         }
     }
--- src/dime/Envelope.cs
+++ src/dime/Envelope.cs
@@ -5,40 +5,64 @@ using System.Collections.Generic;
 
 namespace ShiftEverywhere.DiME
 {
+    ///<summary>Acts as acontainer object for Message instances. Several messages, independent on their origin
+    /// may be added to an envelope. The entity that created the envelope signs it at export and thus sealing
+    /// it's content. </summary>
     public class Envelope
     {
-        /* PUBLIC */
-        public int profile { get; private set; }
-        public Identity identity { get; set; }
-        public List<Message> messages { get; private set; }
-        public Guid subjectId { get { return this.json.sub; } }
-        public Guid issuerId { get { return this.json.iss; } }
-        public long issuedAt { get { return this.json.iat; } }
-        public long expiresAt { get { return this.json.exp; } }
-        public bool isImmutable { get; private set; } = false;
+        #region -- PUBLIC --
+        ///<summary>The cryptographic profile version used for this envelope.</summary>
+        public int Profile { get { return this._profile; } set { this.Reset(); this._profile = value; } }
+        /// <summary>The identity of the issuer, and thus sealer (signer), of the enveloper.</summary>
+        public Identity Identity { get { return this._identity; } set { this.Reset(); this._identity = value; } }
+        /// <summary>A unique identity for the envelope. If an envelope is modfied after it has been sealed, then this id changes.</summary>
+        public Guid Id { get { return this._data.uid; } }
+        /// <summary>A list of messages kept inside the envelope.</summary>
+        public List<Message> Messages { get; private set; }
+        /// <summary>The id of the receiver.</summary>
+        public Guid SubjectId { get { return this._data.sub; } set { this.Reset(); this._data.sub = value; } }
+        /// <summary>The id of the issuer (subject id of the issuer).</summary>
+        public Guid IssuerId { get { return this._data.iss; } set { this.Reset(); this._data.iss = value; } }
+        /// <summary>The timestamp of when the envelope was created (issued).</summary>
+        public long IssuedAt { get { return this._data.iat; } set { this.Reset(); this._data.iat = value; } }
+        /// <summary>The timestamp of when the envelope is expired and is no longer valid.</summary>
+        public long ExpiresAt { get { return this._data.exp; } set { this.Reset(); this._data.exp = value; } }
+        /// <summary>Indicates if the envelope is sealed or not (signed).</summary>
+        public bool IsSealed { get { return this._signature != null; } }
 
-        public Envelope(Identity issuerIdentity, Guid subjectId, long issuedAt, long expiresAt, int profile = Crypto.DEFUALT_PROFILE)
+        /// <summar>Constructs a new Envelope object from the provided parameters. The issued at will be set
+        /// to the current time. </summary>
+        /// <param name="issuerIdentity">The identity of the issuer.</param>
+        /// <param name="subjectId">The id of the receiving subject.</param>
+        /// <param name="validFor">The number of seconds before the envelope expires.</param>
+        /// <param name="profile">The cryptographic profile version to use (optional)</param>
+        public Envelope(Identity issuerIdentity, Guid subjectId, long validFor, int profile = Crypto.DEFUALT_PROFILE)
         {
-            if (!Crypto.SupportedProfile(profile)) { throw new ArgumentException("Unsupported cryptography profile."); }
-            this.identity = issuerIdentity;
-            this.json = new JSONData(Guid.NewGuid(), subjectId, issuerIdentity.SubjectId, issuedAt, expiresAt);
-            this.profile = profile;
+            this.Identity = issuerIdentity;
+            long now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            this._data = new InternalData(Guid.NewGuid(), subjectId, issuerIdentity.SubjectId, now, (now + validFor));
+            this.Profile = profile;
         }
 
+        /// <summary>Creates an envelope object from a DiME encoded string. It will also verify field values and
+        /// signatures before returning a new instance.</summary>
+        /// <param name="encoded">The DiME encoded envelope string to import.</param>
+        /// <exception cref="DataFormatException">If the format of the encoded string is invalid.</exception>
+        /// <exception cref="UnsupportedProfileException">If an invalid cryptographic profile version is set.</exception>
+        /// <exception cref="DateExpirationException">If 'IssuedAt' and/or 'ExpiresAt' contain invalid values, or has the envelope has expired.</exception>
+        /// <exception cref="IntegrityException">If the signature failes validation, or cannot be validated.</exception>
+        /// <returns>An initialized and verified Envelope object.</returns>
         public static Envelope Import(string encoded)
         {
-            if (!encoded.StartsWith(Envelope.HEADER)) { throw new ArgumentException("Unexpected data format."); }
+            if (!Envelope.IsEnvelope(encoded)) { throw new DataFormatException("Unexpected data format."); }
             string[] components = encoded.Split(new char[] { '.' });
-            if (components.Length != 5) { throw new ArgumentException("Unexpected number of components found then decoding identity."); }
+            if (components.Length != 5) { throw new DataFormatException("Unexpected number of components found then decoding identity."); }
             int profile = int.Parse(components[0].Substring(1));
-            if (!Crypto.SupportedProfile(profile)) { throw new ArgumentException("Unsupported cryptography profile."); }
             byte[] identityBytes = Utility.FromBase64(components[1]);
             Identity identity = Identity.Import(System.Text.Encoding.UTF8.GetString(identityBytes, 0, identityBytes.Length));
-            identity.VerifyTrust();
             string envPart = encoded.Substring(0, encoded.LastIndexOf('.'));
             string signature = components[components.Length - 1];
-            Crypto.VerifySignature(profile, envPart, signature, identity.identityKey);
-            Envelope.JSONData parameters = JsonSerializer.Deserialize<Envelope.JSONData>(Utility.FromBase64(components[3]));
+            Envelope.InternalData parameters = JsonSerializer.Deserialize<Envelope.InternalData>(Utility.FromBase64(components[3]));
             Envelope envelope = new Envelope(identity, parameters, profile);
             byte[] msgBytes = Utility.FromBase64(components[2]);
             string[] msgArray = System.Text.Encoding.UTF8.GetString(msgBytes, 0, msgBytes.Length).Split(new char[] { ';' }); ;
@@ -47,46 +71,105 @@ namespace ShiftEverywhere.DiME
                 Message message = Message.Import(msg);
                 envelope.AddMessage(message);
             }
-            envelope.encoded = envPart;
-            envelope.signature = signature;
-            envelope.isImmutable = true;
+            envelope._encoded = envPart;
+            envelope._signature = signature;
+            envelope.Verify();
             return envelope;
         }
 
-        public string Export(string identityPrivateKey = null)
+        /// <summary>This function encodes and exports the envelope object in the DiME format. It will verify 
+        /// the data inside the envelope, as well as the signature attached.</summary>
+        /// <exception cref="UnsupportedProfileException">If an invalid cryptographic profile version is set.</exception>
+        /// <exception cref="DateExpirationException">If 'IssuedAt' and/or 'ExpiresAt' contain invalid values, or has the envelope has expired.</exception>
+        /// <exception cref="IntegrityException">If the signature failes validation, or cannot be validated.</exception>
+        public string Export()
         {
-            if (this.signature == null && identityPrivateKey == null) { throw new ArgumentNullException("Need private key to sign envelope for export."); }
-            if (this.signature == null)
-            {
-                this.encoded = Encode(identityPrivateKey);
-                this.signature = Crypto.GenerateSignature(this.profile, this.encoded, identityPrivateKey);
-                this.isImmutable = true;
-            }
-            Crypto.VerifySignature(this.profile, this.encoded, this.signature, this.identity.identityKey);
-            return this.encoded + "." + this.signature;
+            if (!this.IsSealed) { throw new IntegrityException("Signature missing, unable to export."); }
+            Verify();
+            return Encode() + "." + this._signature;
         }
 
+        /// <summary>This will seal an envelope by signing it using the provided private key (of key type 'Identity').
+        /// The provided private key must be associated with the public key in the Idenity object inside the envelope
+        /// object to be signed.</summary>
+        /// <param name="identityPrivateKey">The private key that should be used to sign the envelope.</param>
+        /// <exception cref="ArgumentNullException">If the passed private key is null.</exception> 
+        public void Seal(string identityPrivateKey)
+        {
+            if (this._signature == null)
+            {
+                if (identityPrivateKey == null) { throw new ArgumentNullException("Private key for signing cannot be null."); }
+                this._signature = Crypto.GenerateSignature(this.Profile, Encode(), identityPrivateKey);
+            }
+        }
+
+        /// <summary>Helper function to quickly check if a string is potentially a DiME encoded envelope object.</summary>
+        /// <param name="encoded">The string to validate.</param>
+        /// <returns>An indication if the string is a DiME encoded envelope.</returns>
+        public static bool IsEnvelope(string encoded)
+        {
+            return encoded.StartsWith(Envelope._HEADER);
+        }
+
+        /// <summary>Adds a message to the envelope. Call to this function will reset the envelope and it
+        /// needs to be sealed again. Messages must have been sealed before being added to an envelope.</summary>
+        /// <param name="message">The message object to add to the envelope</param>
+        /// <exception cref="IntegrityException">If the message added is not first sealed.</exception>
         public void AddMessage(Message message)
         {
-            if (!this.isImmutable)
-            {
-                if (this.messages == null)
-                {
-                    this.messages = new List<Message>();
-                }
-                this.messages.Add(message);
-            }
-            else
-            {
-                throw new ImmutableException();
-            }
+            if (!message.IsSealed) { throw new IntegrityException("Message must be sealed before being added to an envelope."); }
+            this.Reset();
+            if (this.Messages == null) { this.Messages = new List<Message>(); }
+            this.Messages.Add(message);            
         }
 
-        /* PRIVATE */
-        private const string HEADER = "E";
-        private string signature;
-        private string encoded;
-        private struct JSONData
+        /// <summary>Will remove all messages in the envelope. Call to this function will reset the envelope and it
+        /// needs to be sealed again.</summary>
+        public void RemoveAllMessages()
+        {
+            this.Reset();
+            this.Messages = null;
+        }
+
+        /// <summary>Will verify the data in the fields in the evelope object. It will also verify all underlaying
+        /// objects if 'shallowVerification' is set to true (or omitted). The signature of the envelope object will
+        /// be verified with the public key from the 'Identity' property.</summary>
+        /// <param name="shallowVerification">Indicate if a deep verification of all encapsulated objects should be skipped.</param>
+        /// <exception cref="UnsupportedProfileException">If an invalid cryptographic profile version is set.</exception>
+        /// <exception cref="DateExpirationException">If 'IssuedAt' and/or 'ExpiresAt' contain invalid values, or has the envelope has expired.</exception>
+        /// <exception cref="IntegrityException">If the signature failes validation, or cannot be validated.</exception>
+        public void Verify(bool shallowVerification = false)
+        {
+            if (!Crypto.SupportedProfile(this.Profile)) { throw new UnsupportedProfileException("Unsupported cryptography profile version."); }
+            // Verify IssuedAt and ExpiresAt
+            long now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            if (this.IssuedAt > now) { throw new DateExpirationException("Issuing date in the future."); }
+            if (this.IssuedAt > this.ExpiresAt) { throw new DateExpirationException("Expiration before issuing date."); }
+            if (this.ExpiresAt < now) { throw new DateExpirationException("Passed expiration date."); }
+            // Verify underlaying objects, if requested
+            if (!shallowVerification)
+            {
+                this.Identity.VerifyTrust();
+                foreach(Message message in this.Messages)
+                {
+                    //message.Verify();
+                }
+            }
+            // Verify signature
+            if (this._signature == null) { throw new IntegrityException("Signature missing."); }
+            if (this.Identity.SubjectId == this.IssuerId) { throw new IntegrityException("Issuing identity subject id does not match issuer id of the envelope."); }
+            Crypto.VerifySignature(this.Profile, Encode(), this._signature, this.Identity.identityKey);
+        }
+
+        #endregion
+        #region -- PRIVATE --
+
+        private const string _HEADER = "E";
+        private int _profile;
+        private Identity _identity;
+        private string _signature;
+        private string _encoded;
+        private struct InternalData
         {
             public Guid uid { get; set; }
             public Guid sub { get; set; }
@@ -94,7 +177,7 @@ namespace ShiftEverywhere.DiME
             public long iat { get; set; }
             public long exp { get; set; }
 
-            public JSONData(Guid uid, Guid sub, Guid iss, long iat, long exp)
+            public InternalData(Guid uid, Guid sub, Guid iss, long iat, long exp)
             {
                 this.uid = uid;
                 this.sub = sub;
@@ -103,39 +186,46 @@ namespace ShiftEverywhere.DiME
                 this.exp = exp;
             }
         }
-        private Envelope.JSONData json;
+        private Envelope.InternalData _data;
 
-        private Envelope(Identity issuerIdentity, Envelope.JSONData parameters, int profile = Crypto.DEFUALT_PROFILE)
+        private Envelope(Identity issuerIdentity, Envelope.InternalData parameters, int profile = Crypto.DEFUALT_PROFILE)
         {
-            this.identity = issuerIdentity;
-            this.json = parameters;
-            this.profile = profile;
+            this.Identity = issuerIdentity;
+            this._data = parameters;
+            this.Profile = profile;
         }
 
-        private string Encode(string issuerIdentityPrivateKey = null)
+        private string Encode()
         {
-            if ( this.encoded == null ) 
+            if (this._encoded == null) 
             {  
+                // TODO: verify all values (messages == null ??)
                 var envBuilder = new StringBuilder();
                 envBuilder.AppendFormat("{0}{1}.{2}.", 
-                                    Envelope.HEADER,
-                                    this.profile,
-                                    Utility.ToBase64(this.identity.Export()));
+                                    Envelope._HEADER,
+                                    this.Profile,
+                                    Utility.ToBase64(this.Identity.Export()));
                 var msgBuilder = new StringBuilder();
-                foreach (Message message in this.messages)
+                foreach (Message message in this.Messages)
                 {
-                    string msg = message.HasSignature() ? message.Export() : message.Export(issuerIdentityPrivateKey);
-                    msgBuilder.AppendFormat("{0};", msg);
+                    msgBuilder.AppendFormat("{0};", message.Export());
                 }
                 msgBuilder.Remove(msgBuilder.Length - 1, 1); 
                 envBuilder.AppendFormat("{0}.{1}",
                                         Utility.ToBase64(msgBuilder.ToString()),
-                                        Utility.ToBase64(JsonSerializer.Serialize(this.json)));
-                this.encoded = envBuilder.ToString();
+                                        Utility.ToBase64(JsonSerializer.Serialize(this._data)));
+                this._encoded = envBuilder.ToString();
             }
-            return this.encoded;
+            return this._encoded;
         }
 
+        private void Reset()
+        {
+            this._data.uid = Guid.NewGuid();
+            this._encoded = null;
+            this._signature = null;
+        }
+        #endregion
     }
 
 }
